2b00640ddc0b741bc7135e17caa96f66f37f8652
Nwoha_Martins <martinsmind5@gmail.com>
2023-02-18 11:11:28 -0800

    5-print_numbers.c, 6-print_numberz.c,  7-print_tebahpla.c, 8-print_base16.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+*  main - prints the alphabet in lowercase
+*
+* Return: character
+*/
+int main(void)
+{
+	int c = 48;
+
+	while (c >= 48 && c <= 57)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <math.h>
+/* more headers goes there */
+
+/**
+*  main -  prints all the numbers of base 16 in lowercase
+*
+* Return: base 16 numbers
+*/
+int main(void)
+{
+	int n = 48;
+
+	while (n >= 48 && n <= 102)
+	{
+		putchar(n);
+		if (n == 57)
+			n = 96;
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
