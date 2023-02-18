03296a6384d5e7a3bdaff0fc6fb9670036473154
Nwoha_Martins <martinsmind5@gmail.com>
2023-02-18 11:38:28 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <math.h>
+/**
+*  main -  prints all possible combinations of single-digit numbers
+*
+* Return: numbers separate by goma
+*/
+int main(void)
+{
+	int n = 48;
+
+	int m = 49;
+
+	int j = 0, i = 0, a = 8;
+
+	while (i  <= a && a != -1)
+	{
+		i++;
+		while (j <= a)
+		{
+			putchar(n);
+			putchar(m);
+			if (n != 56 || m != 57)
+			{
+				putchar(',');
+				putchar(',');
+			}
+			m++;
+			j++;
+		}
+		n++;
+		m = m - a;
+		a--;
+		i = 0;
+		j = 0;
+	}
+	putchar('\n');
+	return (0);
+}
