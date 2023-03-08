d7e56131ec52cb8b236e876e83b35558716774c5
Nwoha_Martins <martinsmind5@gmail.com>
2023-03-08 10:57:06 -0800

    Write a function that prints a string, followed by a new line. also added a README file

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int nat_square(int i, int j);
+int is_prime_number(int n);
+int primeNum(int num, int prime);
+int is_palindrome(char *s);
+int string_len(char *s);
+int recur(char *s, int l);
+int wildcmp(char *s1, char *s2);
+
+#endif /* MAIN_H */
