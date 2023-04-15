6dd2acca7d900902d2b892d44aab2d813dc01bec
Nwoha_Martins <martinsmind5@gmail.com>
2023-04-14 19:20:53 -0700

    Write a program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -51,14 +51,10 @@ void close_file(int fd)
  *
  * Return: 0 on success.
  *
- * Description: If the argument count is incorrect - 
- * 		exit code 97.
- *              If file_from does not exist or cannot be read - 
- *              exit code 98.
- *              If file_to cannot be created or written to - 
- *              exit code 99.
- *              If file_to or file_from cannot be closed - 
- *              exit code 100.
+ * Description: If the argument count is incorrect - exit code 97.
+ *              If file_from does not exist or cannot be read - exit code 98.
+ *              If file_to cannot be created or written to - exit code 99.
+ *              If file_to or file_from cannot be closed -  exit code 100.
  */
 int main(int argc, char *argv[])
 {
