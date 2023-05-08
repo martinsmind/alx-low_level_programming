e023d9f2e25c3495ee8247b73cad5a93585611af
Nwoha Martins <martinsmind5@gmail.com>
2023-05-08 16:31:21 +0100

    Created main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,18 +2,18 @@
 #define MAIN_H
 
 /*
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x14-file_io directory.
+ * Desc: The header file containing prototypes of functions
+ *       written in the file directory.
  */
 
 #include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/stat.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
