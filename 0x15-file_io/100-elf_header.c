4e45e5df0e49c59920c5744fccf189e23b1a859c
Nwoha Martins <martinsmind5@gmail.com>
2023-05-08 17:55:16 +0100

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,10 +1,10 @@
 #include <elf.h>
 #include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 void check_elf(unsigned char *e_ident);
 void print_data(unsigned char *e_ident);
